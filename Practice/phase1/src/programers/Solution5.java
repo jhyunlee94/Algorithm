8f77eb6d70a8423732c15a7f0d1b0a37de2762f4
jhyunlee94 <jkwhd12@gmail.com>
2022-07-13 19:08:30 +0900

    0713

--- Practice/phase1/src/programers/Solution5.java
+++ Practice/phase1/src/programers/Solution5.java
@@ -5,6 +5,7 @@ public class Solution5 {
     public static void main(String[] args) {
         Solution5 solution5 = new Solution5();
         System.out.println(solution5.solution(113));
+
     }
 
 //    private boolean solution(int x) {
