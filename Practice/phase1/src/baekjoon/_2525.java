d6d4bdec44bfeb518c5d7178defa08a6c36b066d
jhyunlee94 <jkwhd12@gmail.com>
2022-07-15 18:34:48 +0900

    0715

--- Practice/phase1/src/baekjoon/_2525.java
+++ Practice/phase1/src/baekjoon/_2525.java
@@ -10,10 +10,12 @@ public class _2525 {
         int B = sc.nextInt();
         int C = sc.nextInt();
 
+        //전부 다 분으로 변경하고
         int test = (A * 60) + B + C;
         A = test/60; // 시
         B = test%60; // 분
 
+        //만약 시간이 24시간이 넘는다면 0으로
         if(A >= 24 ) {
             A = A - 24;
         }
